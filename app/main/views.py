eac72e7edabdbc780175ffb7f14e4e9c657372c9
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-09-22 00:13:36 +0300

    add functionality for rendering index.html

--- app/main/views.py
+++ app/main/views.py
@@ -6,3 +6,20 @@ from .forms import UpdateProfile,PitchForm,CommentForm
 from flask_login import login_required,current_user
 #import datetime
 
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to Pitch'
+    # Getting reviews by category
+    interview_pitches = Pitch.get_pitches('interview')
+    product_pitches = Pitch.get_pitches('product')
+    promotion_pitches = Pitch.get_pitches('promotion')
+
+
+    return render_template('index.html',title = title, interview = interview_pitches, product = product_pitches, promotion = promotion_pitches)
+
